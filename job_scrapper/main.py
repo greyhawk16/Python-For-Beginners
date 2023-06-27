198f61d663e36f4e9412a19e174a0ab6b633f593
grey bird <hitec16@naver.com>
2023-06-28 08:49:50 +0900

    Starting web scrapper project

---
+++ job_scrapper/main.py
@@ -0,0 +1,11 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+base_url = "https://weworkremotely.com/remote-jobs/search?term="
+search_term = 'python'
+response = get(f"{base_url}{search_term}")
+
+if response.status_code != 200:
+    print("Can't request website")
+else:
+    print(response.text)
