caef7c720a28d6a0db7f24b3120ea3a324e0efb1
grey bird <hitec16@naver.com>
2023-06-28 09:01:01 +0900

    Added Bueatifulsoup to find class 'jobs' in base_url

--- job_scrapper/main.py
+++ job_scrapper/main.py
@@ -8,4 +8,5 @@ response = get(f"{base_url}{search_term}")
 if response.status_code != 200:
     print("Can't request website")
 else:
-    print(response.text)
+    soup = BeautifulSoup(response.text, "html.parser")
+    jobs = soup.find_all('section', class_='jobs')
