064b38fb5f29b11d149e0317dc5ca61f728dbf33
grey bird <hitec16@naver.com>
2023-06-29 10:01:20 +0900

    retrieve job sections without view-all buttons

--- job_scrapper/main.py
+++ job_scrapper/main.py
@@ -5,8 +5,15 @@ base_url = "https://weworkremotely.com/remote-jobs/search?term="
 search_term = 'python'
 response = get(f"{base_url}{search_term}")
 
+
 if response.status_code != 200:
     print("Can't request website")
 else:
     soup = BeautifulSoup(response.text, "html.parser")
     jobs = soup.find_all('section', class_='jobs')
+    for job_section in jobs:
+        job_posts = job_section.find_all("li")
+        job_posts.pop()
+        for post in job_posts:
+            print(post)
+            print("///////")
