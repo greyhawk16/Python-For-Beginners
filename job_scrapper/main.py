b5a766bbe947eed55d38d9dae9d39884863112a9
grey bird <hitec16@naver.com>
2023-06-30 16:30:24 +0900

    Seperated function which scrap website

--- job_scrapper/main.py
+++ job_scrapper/main.py
@@ -1,24 +1,7 @@
 from requests import get
 from bs4 import BeautifulSoup
+from extractors.wwr import extract_wwr_jobs
 
-base_url = "https://weworkremotely.com/remote-jobs/search?term="
-search_term = 'python'
-response = get(f"{base_url}{search_term}")
-
-
-if response.status_code != 200:
-    print("Can't request website")
-else:
-    soup = BeautifulSoup(response.text, "html.parser")
-    jobs = soup.find_all('section', class_='jobs')
-    for job_section in jobs:
-        job_posts = job_section.find_all("li")
-        job_posts.pop()
-        for post in job_posts:
-            anchors = post.find_all('a')
-            anchor = anchors[1]
-            link = anchor['href']
-            company, kind, region = anchor.find_all('span', class_="company")
-            title = anchor.find('span', class_='title')
-            print(company, kind, region, title)
-            print('-------------')
+jobs = extract_wwr_jobs('python')
+for j in jobs:
+    print(j) 
