83a4ec1f1ffef1d2bea73a878bef537a143ed02a
grey bird <hitec16@naver.com>
2023-07-01 07:16:54 +0900

    Added Selinium

--- job_scrapper/main.py
+++ job_scrapper/main.py
@@ -1,7 +1,21 @@
 from requests import get
 from bs4 import BeautifulSoup
 from extractors.wwr import extract_wwr_jobs
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-jobs = extract_wwr_jobs('python')
-for j in jobs:
-    print(j) 
+
+options = Options()
+options.add_experimental_option("detach", True)
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+
+# base_url = "https://www.indeed.com/jobs?q="
+# search_term = "python"
+# response = get(f"{base_url}{search_term}")
+
+browser = webdriver.Chrome(options=options)
+browser.get(
+    'https://kr.indeed.com/jobs?q=python&l=&from=searchOnHP&vjk=89395b6ac5014113')
+
+print(browser.page_source)
