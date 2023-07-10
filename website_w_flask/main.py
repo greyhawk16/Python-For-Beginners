713819b81f34a834adda496442911eea0ff89b70
grey bird <hitec16@naver.com>
2023-07-10 11:10:56 +0900

    request.get(keyword) giving NoneType.

--- website_w_flask/main.py
+++ website_w_flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder="templates")
 base_url = '127.0.0.1'
@@ -6,12 +6,15 @@ base_url = '127.0.0.1'
 
 @app.route("/")
 def home():
-    return render_template("home.html", name='DDG')
+    return render_template("home.html")
 
 
 @app.route("/search")
 def hello():
-    return render_template("search.html")
+    print('request.args: ', request.args)
+    print('keyword:      ', request.args.get("keyword"))
+    keyword = request.args.get("keyword")
+    return render_template("search.html", keyword=keyword)
 
 
-app.run(base_url, debug=True)
+app.run(base_url)
